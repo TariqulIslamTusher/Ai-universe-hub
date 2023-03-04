6d21b356f36725c5190f1ed9a9f8d4abe7802abc
TariqulIslamTusher <islamtariqul652@gmail.com>
2023-03-04 12:59:06 +0600

    Added the sorting function

--- script/script.js
+++ script/script.js
@@ -1,49 +1,38 @@
 // Declare the first fatching function
-
-const loadData = async (isLimit) =>{
-    try{
+let universal;
+const loadData = async () => {
+    try {
         const URL = "https://openapi.programming-hero.com/api/ai/tools"
         const res = await fetch(URL);
         const data = await res.json()
-        displayData(data.data.tools, isLimit )
-    } catch(error) {
+        universal = data.data.tools;
+        displayData(data.data.tools.slice(0, 6))
+    } catch (error) {
         console.log(error, 'check on loadData function')
     }
 }
 
 // Declare first function to display the url into card
-
-const displayData = (data , isLimit) =>{
-    // including see more button condition
-    if(isLimit && data.length > 6){
-        data = data.slice(0,6)
-        document.getElementById('seeMore').classList.remove('hidden')
-    } else{
-        document.getElementById('seeMore').classList.add('hidden')
-    }
-
+const displayData = (data) => {
     const cardContainer = document.getElementById('card-container')
     cardContainer.innerHTML = ""
-    
-    // apply foreach for the array named data
+
+    // TODO: apply foreach for the array named data
     data.forEach(element => {
 
         // destructing the object 
-        const {id, name, published_in, image, features} = element;
+        const { id, name, published_in, image, features } = element;
         const cardDiv = document.createElement('div')
-        
-
         cardDiv.innerHTML = `
-            <div class="card w-full h-full p-6 bg-base-100 shadow-2xl border hover:border-4:border-blue-700">
+            <div class="card w-full h-full p-6 bg-base-100 shadow-2xl border-4 border-blue-200 hover:border-blue-800 delay-75">
                 <figure>
-                    <img class='md:h-52 md:w-full' src="${image? image:"No Photos Found"}" alt="Shoes" />
+                    <img class='md:h-52 md:w-full' src="${image ? image : "No Photos Found"}" alt="Shoes" />
                 </figure>
 
                 <div class="card-body px-0 border-b-4">
                     <h2 class="card-title text-4xl font-semibold my-3">Features</h2>
-                    <ol class='list-decimal px-4' type="1">${features.map(feature=>`
-                        <li>${feature}</li>
-                                            `).join('')}
+                    <ol class='list-decimal px-4' type="1">
+                        ${features.map(feature => `<li>${feature}</li>`).join('')}
                     </ol>
                 
                 </div>
@@ -51,79 +40,108 @@ const displayData = (data , isLimit) =>{
                 <div class="card-footer py-3 flex justify-between items-center">
                     <div class="flex flex-col">
                         <h2 class="my-3 text-4xl font-semibold">${name}</h2>
-                        <div class="flex"><p class="text-xl"><i class="fa-regular fa-calendar-days mr-3"></i><span id="date">${published_in}</span></p></div>
+                        <div class="flex"><p class="text-2xl"><i class="fa-regular fa-calendar-days mr-3"></i><span id="date">${published_in}</span></p></div>
                     </div>
 
-                    <label onclick="receivedModalClick('${id}')" for="my-modal-5" class="btn bg-yellow-300 border-0 rounded-2xl px-4 py-3"><i class="fa-solid fa-arrow-right"></i></label>
+                    <label onclick="receivedModalClick('${id}')" for="my-modal-5" class="btn bg-transparent text-black border-2 rounded-full text-2xl font-bold px-4 py-3 align-middle hover:text-white"><i class="fa-solid fa-arrow-right"></i></label>
                 </div>
             </div>
         `
-        cardContainer.appendChild(cardDiv)        
+        cardContainer.appendChild(cardDiv)
     });
-    
+
     spinners()
 }
+
+
 // create the modal display functions
-const receivedModalClick = id =>{
+const receivedModalClick = id => {
     fetch(`https://openapi.programming-hero.com/api/ai/tool/${id}`)
-    .then(res => res.json())
-    .then(data => openModal(data.data))
+        .then(res => res.json())
+        .then(data => openModal(data.data))
 }
 
 // function for open a modal
 const openModal = id => {
-    const modalContainer = document.getElementById('modal-container')
-    
     // destruction of object id 
-    const {description, accuracy, image_link, pricing, input_output_examples, features, integrations} = id
+    const { description, accuracy, image_link, pricing, input_output_examples, features, integrations } = id
+
+    //! remove later
+            const arrayOfArray = Object.entries(features)
+                const eachArray = arrayOfArray.map(feature=>feature[1].feature_name)
+            console.log(eachArray);
+    //! remove later
 
     document.getElementById('description').innerText = description
-    document.getElementById('figure').innerHTML =`
+    document.getElementById('figure').innerHTML = `
         <img class="md:h-full w-auto my-3" src=${image_link[0]}>
-        <h2 class='absolute right-0 top-1 bg-red-500 text-xl text-white px-1'>${accuracy.score? `${accuracy.score}% accuracy` :""}</h2>
+        <h2 class='absolute right-0 top-1 bg-red-500 text-xl text-white px-1'>${accuracy.score ? `${accuracy.score}% accuracy` : ""}</h2>
     `
-    document.getElementById('pricing').innerHTML =`
-        <div class="p-3 py-5 text-center content-center shadow-lg text-lg h-full bg-white rounded-md w-full leading-normal text-green-500"><p>${pricing? pricing[0].price : "Free Of Cost"}</p>
-        <p>${pricing? pricing[0].plan : ''}</p></div>
-        <div class="p-3 py-5 text-center content-center shadow-lg text-lg h-full bg-white rounded-md w-full leading-normal text-blue-500"><p>${pricing? pricing[1].price : "Free Of Cost"}</p>
-        <p>${pricing? pricing[1].plan : ''}</p></div>
-        <div class="p-2 text-center content-center shadow-lg text-lg h-full bg-white rounded-md w-full leading-normal text-red-500"><p>${pricing? pricing[2].price : "Free Of Cost"}</p>
-        <p>${pricing? pricing[2].plan : ''}</p></div>
+    document.getElementById('pricing').innerHTML = `
+        <div class="p-3 py-5 text-center content-center shadow-lg text-lg h-full bg-white rounded-md w-full leading-normal text-green-500">
+            <p>${pricing ? pricing[0].price : "Free Of Cost"}</p>
+            <p>${pricing ? pricing[0].plan : ''}</p>
+        </div>
+
+        <div class="p-3 py-5 text-center content-center shadow-lg text-lg h-full bg-white rounded-md w-full leading-normal text-blue-500">
+            <p>${pricing ? pricing[1].price : "Free Of Cost"}</p>
+            <p>${pricing ? pricing[1].plan : ''}</p>
+        </div>
+
+        <div class="p-2 text-center content-center shadow-lg text-lg h-full bg-white rounded-md w-full leading-normal text-red-500">
+            <p>${pricing ? pricing[2].price : "Free Of Cost"}</p>
+            <p>${pricing ? pricing[2].plan : ''}</p>
+        </div>
 
     `
-    document.getElementById('input').innerText = input_output_examples? input_output_examples[0].input : "Can you give any example?";
-    document.getElementById('output').innerText = input_output_examples? input_output_examples[0].output : "No! Not Yet! Take a break!!!";
+    document.getElementById('input').innerText = input_output_examples ? input_output_examples[0].input : "Can you give any example?";
+    document.getElementById('output').innerText = input_output_examples ? input_output_examples[0].output : "No! Not Yet! Take a break!!!";
+
     // features informations
-    document.getElementById('olFeatures').innerHTML = `
-        ${features['1']? `<li>${features['1'].feature_name}</li>` : ""}
-        ${features['2']? `<li>${features['2'].feature_name}</li>` : ""}
-        ${features['3']? `<li>${features['3'].feature_name}</li>` : ""}
-        ${features['4']? `<li>${features['4'].feature_name}</li>` : ""}
+    document.getElementById('olFeatures').innerHTML = `${features? eachArray.map(name=> `<li>${name}</li>` ).join(''):'No data found'}
     `
     // Intigrations informations
-    document.getElementById('olIntigrations').innerHTML = `${integrations.map(integration =>
-        `<li>${integration}</li>`).join("")}`
-        
-    
+    document.getElementById('olIntigrations').innerHTML = `${integrations ? integrations.map(integration =>
+        `<li>${integration ? integration : "No Data Found"}</li>`).join(""):"No Data Found" }`
 }
 
 
 
 // create a spinners function
-const spinners = (isSpin)=>{
-    if(isSpin){
+const spinners = (isSpin) => {
+    if (isSpin) {
         document.getElementById('spinner').classList.remove('hidden')
-    } else{
+    } else {
         document.getElementById('spinner').classList.add('hidden')
     }
 }
-// on click show all show all the card
-const showAllCard = () =>{
+
+
+// TODO: on click show all show all the card
+let isClicked = false;
+const showAllCard = async () => {
     spinners(true)
-    loadData(false)
+    displayData(universal)
+    document.getElementById('seeMore').classList.add('hidden')
+    isClicked = true
 }
 
-const showLimitedCard = () => {
-    spinners(true)
-    loadData(true)
-}
+loadData()
+
+
+
+
+document.getElementById('sortDate').addEventListener('click', function () {
+    // document.getElementById('card-container').innerHTML = ""
+    if(isClicked){
+        const sortedData = [...universal]
+        sortedData.sort((p1, p2) => (p1.published_in < p2.published_in) ? -1 : (p1.price > p2.price) ? 1 : 0);
+         displayData(sortedData)
+    }else{
+        const sortedData = [...universal.slice(0,6)]
+        sortedData.sort((p1, p2) => (p1.published_in < p2.published_in) ? -1 : (p1.price > p2.price) ? 1 : 0);
+        displayData(sortedData)
+    }
+    console.log(isClicked);
+    // sortByDate(data)
+})
--- script/test.js
+++ script/test.js
@@ -1,4 +1,66 @@
-const features = [4, 3, 6, 6, 2, 6, 7,2,92]
+// let products = [
+//     {
+//       "product_name": "The Witchers",
+//       "type": "book",
+//       "manufactured": new Date('2019-05-13'),
+//       "price": 800
+//     },
+//     {
+//       "product_name": "Black Heels",
+//       "type": "Shoes",
+//       "manufactured": new Date('2021-07-06'),
+//       "price": 2500
+//     },
+//     {
+//       "product_name": "Skybags",
+//       "type": "Bags",
+//       "manufactured": new Date('2020-09-22'),
+//       "price": 2200
+//     },
+//     {
+//       "product_name": "OnePlus 9",
+//       "type": "Mobile Phone",
+//       "manufactured": new Date('2021-03-23'),
+//       "price": 49000
+//     },
+//   ]
+//   console.log("Original Products are:")
+//   console.log(products)
+//   let sortedProducts = products.sort(
+//       (p1, p2) => (p1.price < p2.price) ? 1 : (p1.price > p2.price) ? -1 : 0);
+  
+//   console.log("Products sorted based on descending order of their prices are:")
+//   console.log(sortedProducts);
+  
+let tools = [{id: "01", price:700, name: "ChatGPT"},
 
-let map = features.map(feature => `<li>${feature}</li>`).join('')
-console.log(map)
+    {id: "02", price:410, name: "YouChat 2.0", description: null},
+
+    {id: "03", price:54, name: "Google Bard", description: "Bard is an alternative developed by Google."},
+
+    {id: "04", price:542, name: "Microsoft Bing", description: null},
+
+    {id: "05", price:452, name: "Chatsonic", description: null},
+
+    {id: "06", price:4025, name: "Jasper Chat", description: "Jasper Chat is an open-source chatbot platform."},
+  
+    {id: "07", price:42, name: "Character AI"},
+
+    {id: "08", price:0788, name: "DialoGPT"},
+
+    {id: "09", price:778678300, name: "Rytr"},
+
+    {id: "10", price:71420785400, name: "GitHub Copilot"},
+
+
+    {id: "11", price:77254, name: "Replika"},
+
+    {id: "12", price:74520, name: "Facebook AI"}
+]
+console.log("Original Products are:")
+console.log(tools)
+let sortedTools = tools.sort(
+    (p1, p2) => (p1.price < p2.price) ? -1 : (p1.price > p2.price) ? 1 : 0);
+
+console.log("Products sorted based on descending order of their prices are:")
+console.log(tools);
